be18dae9ea272adeffea1ce1749ba4aa1f724a80
billhong6981 <bill.burton6981@gmail.com>
2019-08-15 22:41:47 +0000

    fix import

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,6 @@ from os import getenv
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from sqlalchemy import Table, ForeignKey
-from models.place import Place
 
 
 class Amenity(BaseModel, Base):
