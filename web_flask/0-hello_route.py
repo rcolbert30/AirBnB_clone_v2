1ee356ddcd9a7869319e5213c55cf6e642099691
rcolbert30 <colbertrashaad@gmail.com>
2020-01-22 04:37:07 +0000

    web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+starts Flask
+listen: 0.0.00, port 5000
+'''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hbnb_hello():
+    '''
+    display hello if works
+    '''
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
